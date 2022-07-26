b9718075e83e824c19b83543dc7f4c174f463abc
Poorna Gunawardana <poornaanj@gmail.com>
2022-07-26 10:54:38 +0530

    This is the code after refactoring the mortgage calculator

--- src/com/poorna/Main.java
+++ src/com/poorna/Main.java
@@ -8,56 +8,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final byte MONTHS_PER_YEAR = 12;
-        final byte PERCENT = 100;
-        float monthlyInterest;
-        int noOfPayments;
-        double mortgage;
-        int principal;
-        float annualInterest;
-        short period;
+        int principal = (int)readingInput("Enter the amount",100000,10000000);
+        float annualInterest = (float)readingInput("Enter the annual interest rate",0,20);
+        short period = (short)readingInput("Enter the no of years",5,30);
 
-        Scanner scanner = new Scanner(System.in);
-
-        while(true){
-            System.out.print("Principal : ");
-            principal = scanner.nextInt();
-            if(principal<100000||principal>10000000) {
-                System.out.println("Enter a value between 100000 & 10000000");
-                continue;
-            }
-            break;
-        }
-
-        while(true){
-            System.out.print("Annual interest rate : ");
-            annualInterest = scanner.nextFloat();
-            if(annualInterest<0||annualInterest>30) {
-                System.out.println("Enter a value between 0 & 30");
-                continue;
-            }
-            break;
-        }
-
-        while(true){
-            System.out.print("Period (Years) : ");
-            period = scanner.nextShort();
-            if(period<0||period>30) {
-                System.out.println("Enter a value between 0 & 30");
-                continue;
-            }
-            break;
-        }
-
-        monthlyInterest = annualInterest/MONTHS_PER_YEAR/PERCENT;
-        noOfPayments = period*MONTHS_PER_YEAR;
-
-        mortgage = principal*(monthlyInterest*Math.pow((1+monthlyInterest),noOfPayments))/(Math.pow((1+monthlyInterest),noOfPayments)-1);
+        double mortgage = calculateMortgage(principal,annualInterest,period);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
 
         System.out.println("Mortgage : "+ mortgageFormatted);
 
+    }
 
+    public static double readingInput(String message,int min, int max){
+        double input;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.print(message + " : ");
+            input = scanner.nextDouble();
+            if(input>=min && input<=max)
+                break;
+            System.out.println("Enter a value between " + min + " & " + max);
+        }
+        return input;
+    }
+
+    public static double calculateMortgage(int principal,float annualInterest,short period){
+
+        final byte MONTHS_PER_YEAR = 12;
+        final byte PERCENT = 100;
+        float monthlyInterest;
+        int noOfPayments;
+
+        monthlyInterest = annualInterest/MONTHS_PER_YEAR/PERCENT;
+        noOfPayments = period*MONTHS_PER_YEAR;
+
+        return  principal*(monthlyInterest*Math.pow((1+monthlyInterest),noOfPayments))/(Math.pow((1+monthlyInterest),noOfPayments)-1);
     }
 }
