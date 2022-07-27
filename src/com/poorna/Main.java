7661766ea333dcdc69ba3db8bba24baa9fb66656
Poorna Gunawardana <poornaanj@gmail.com>
2022-07-27 19:08:05 +0530

    Refactored payment schedule

--- src/com/poorna/Main.java
+++ src/com/poorna/Main.java
@@ -6,29 +6,38 @@ import java.util.Scanner;
 
 public class Main {
 
+    static final byte MONTHS_PER_YEAR = 12;
+    static final byte PERCENT = 100;
+
     public static void main(String[] args) {
 
         int principal = (int)readingInput("Enter the amount",100000,10000000);
         float annualInterest = (float)readingInput("Enter the annual interest rate",0,20);
         short period = (short)readingInput("Enter the no of years",5,30);
 
-        double mortgage = calculateMortgage(principal,annualInterest,period);
+        printMortgage(principal, annualInterest, period);
 
+        printPaymentSchedule(principal, annualInterest, period);
+
+    }
+
+    private static void printMortgage(int principal, float annualInterest, short period) {
+        double mortgage = calculateMortgage(principal, annualInterest, period);
         String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
-
         System.out.println("Mortgage");
         System.out.println("---------");
         System.out.println("Monthly Payment : "+ mortgageFormatted);
-        System.out.println(" ");
+    }
+
+    private static void printPaymentSchedule(int principal, float annualInterest, short period) {
+        System.out.println();
         System.out.println("Payment schedule");
         System.out.println("----------------");
-
-        for(short paymentsMade=1;paymentsMade<=(period*12);paymentsMade++){
-            double remainingBalance = calculatePaymentSchedule(principal,annualInterest,period,paymentsMade);
+        for(short month = 1; month<=(period *MONTHS_PER_YEAR); month++){
+            double remainingBalance = calculateBalance(principal, annualInterest, period,month);
             String remainingBalanceFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(remainingBalance);
             System.out.println(remainingBalanceFormatted);
         }
-
     }
 
     public static double readingInput(String message,int min, int max){
@@ -48,27 +57,17 @@ public class Main {
 
     public static double calculateMortgage(int principal,float annualInterest,short period){
 
-        final byte MONTHS_PER_YEAR = 12;
-        final byte PERCENT = 100;
-        float monthlyInterest;
-        int noOfPayments;
-
-        monthlyInterest = annualInterest/MONTHS_PER_YEAR/PERCENT;
-        noOfPayments = period*MONTHS_PER_YEAR;
+        float monthlyInterest = annualInterest/MONTHS_PER_YEAR/PERCENT;
+        int noOfPayments = period*MONTHS_PER_YEAR;
 
         return  principal*(monthlyInterest*Math.pow((1+monthlyInterest),noOfPayments))/(Math.pow((1+monthlyInterest),noOfPayments)-1);
     }
 
-    public static double calculatePaymentSchedule(int principal, float annualInterest, short period, short paymentsMade){
+    public static double calculateBalance(int principal, float annualInterest, short period, short numberOfPaymentsMade){
 
-        final byte MONTHS_PER_YEAR = 12;
-        final byte PERCENT = 100;
-        float monthlyInterest;
-        int noOfPayments;
+        float monthlyInterest = annualInterest/MONTHS_PER_YEAR/PERCENT;
+        int noOfPayments = period*MONTHS_PER_YEAR;
 
-        monthlyInterest = annualInterest/MONTHS_PER_YEAR/PERCENT;
-        noOfPayments = period*MONTHS_PER_YEAR;
-
-        return principal*(Math.pow((1+monthlyInterest),noOfPayments)-Math.pow((1+monthlyInterest),paymentsMade))/(Math.pow((1+monthlyInterest),noOfPayments)-1);
+        return principal*(Math.pow((1+monthlyInterest),noOfPayments)-Math.pow((1+monthlyInterest),numberOfPaymentsMade))/(Math.pow((1+monthlyInterest),noOfPayments)-1);
     }
 }
