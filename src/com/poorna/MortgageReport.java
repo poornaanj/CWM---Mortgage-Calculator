361d2313dcc11d71da0ee9b6ac9e37e0d84cf1be
Poorna Gunawardana <poornaanj@gmail.com>
2022-07-30 16:02:56 +0530

    Refactored currency formatting object

--- src/com/poorna/MortgageReport.java
+++ src/com/poorna/MortgageReport.java
@@ -6,13 +6,15 @@ import java.util.Locale;
 public class MortgageReport {
 
     private MortgageCalculator calculator;
+    private final NumberFormat currency;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance(Locale.US);
     }
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
+        String mortgageFormatted = currency.format(mortgage);
         System.out.println("Mortgage");
         System.out.println("---------");
         System.out.println("Monthly Payment : " + mortgageFormatted);
@@ -24,7 +26,7 @@ public class MortgageReport {
         System.out.println("----------------");
 
         for(double balance: calculator.getRemainingBalance())
-            System.out.println(NumberFormat.getCurrencyInstance(Locale.US).format(balance));
+            System.out.println(currency.format(balance));
 
     }
 }
